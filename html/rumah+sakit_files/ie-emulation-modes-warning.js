d4457c0b800488cf1352098040bf5dde8b6e21b2
ihfazhillah <mihfazhillah@gmail.com>
2016-04-15 21:56:32 +0700

    menambah file html untuk offline unittesting

---
+++ html/rumah+sakit_files/ie-emulation-modes-warning.js
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1256" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="moh rifai">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Qaamus Bahasa Arab </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!--script type="text/javascript" src="http://qaamus.com/rating/js/jquery-1.7.1.min.js"></script-->	
+    <script type="text/javascript" src="http://qaamus.com/rating/js/jquery.5stars.min.js"></script>	
+    <!-- Bootstrap core CSS -->
+    <link href="http://qaamus.com/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://qaamus.com/custom.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="http://qaamus.com/js/ie-emulation-modes-warning.js"></script>
+
+        <script type="text/javascript">
+                $(document).ready(function(){
+                        $('.stars').rating({
+                                skin: 'http://qaamus.com/rating/skins/skin.png',          //use full path like 'www.mysite.com/path_to/skin.png'
+                                php: 'http://qaamus.com/rating/admin/php/manager.php',    //use full path like 'www.mysite.com/path_to/manager.php'
+                                debug: true                     //will show message in case of erors, for debugging only
+                        });
+                });
+        </script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://qaamus.com">Qaamus.com</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+
+            <li class=""><a href="http://qaamus.com/indonesia-arab.php">Kamus</a></li>
+			<li class=""><a href="http://qaamus.com/nama.php">Terjemah Nama (Pego)</a></li>			
+			<li class=""><a href="http://qaamus.com/angka.php">Terjemah Angka</a></li>
+			<li class=""><a href="http://qaamus.com/peribahasa-bahasa-arab/1">Peribahasa</a></li>			
+		  <!--li><a href="#">Tata Bahasa Arab</a></li-->
+			
+            
+
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informasi <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+<li class=""><a href="http://qaamus.com/about.php">Tentang kami</a></li>
+            <li class=""><a href="http://qaamus.com/penggunaan.php">Penggunaan</a></li>
+                <li class=""><a href="http://qaamus.com/contact.php">Kontak</a></li>
+
+
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+ 
+<center>Tidak ditemukan</center>
