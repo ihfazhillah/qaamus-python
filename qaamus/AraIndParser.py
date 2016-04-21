c3d933ef15780529121f6f557d258ea0013bcefb
ihfazhillah <mihfazhillah@gmail.com>
2016-04-21 19:29:44 +0700

    memasukkan semua fungsi ke IndAraParser class

--- qaamus/AraIndParser.py
+++ qaamus/AraIndParser.py
@@ -10,57 +10,57 @@ class IndAraParser:
     def __init__(self, soup):
         self.soup = soup
 
-def get_ara_master(soup):
-    result = soup.select("center > .lateef2")
-    return result[0].text
+    def get_ara_master(self):
+        result = self.soup.select("center > .lateef2")
+        return result[0].text
 
-def get_ind_master(soup):
-    result = soup.select(".panel-heading > h3 > .label")
-    return result[0].text
+    def get_ind_master(self):
+        result = self.soup.select(".panel-heading > h3 > .label")
+        return result[0].text
 
-def get_footer_master(soup):
-    result = soup.select(".panel-footer")
-    return result[0].text
+    def get_footer_master(self):
+        result = self.soup.select(".panel-footer")
+        return result[0].text
 
-def get_arti_berhub(soup):
-    ind = [x.text for x in soup.select("td > a")]
-    ara = [x.text for x in soup.select("td.lateef")]
-    result =[{"ind":ind[i], "ara":ara[i]}for i in range(len(ind))] 
-    return result
+    def get_arti_berhub(self):
+        ind = [x.text for x in self.soup.select("td > a")]
+        ara = [x.text for x in self.soup.select("td.lateef")]
+        result =[{"ind":ind[i], "ara":ara[i]}for i in range(len(ind))] 
+        return result
 
-def get_arti_master(soup):
-    result = {"ind": get_ind_master(soup),
-              "ara": get_ara_master(soup),
-              "footer": get_footer_master(soup)
-             }
-    return result
+    def get_arti_master(self):
+        result = {"ind": self.get_ind_master(),
+                  "ara": self.get_ara_master(),
+                  "footer": self.get_footer_master()
+                 }
+        return result
 
-def get_next_page_url(soup):
-    #http://stackoverflow.com/questions/9007653/how-to-find-tag-with-particular-text-with-beautiful-soup
-    find_next = soup.find("a", text="Next »")
-    if find_next:
-        return find_next['href']
-    return False
+    def get_next_page_url(self):
+        #http://stackoverflow.com/questions/9007653/how-to-find-tag-with-particular-text-with-beautiful-soup
+        find_next = self.soup.find("a", text="Next »")
+        if find_next:
+            return find_next['href']
+        return False
 
-def get_all_arti_berhub(soup, soupping):
-    """
-    ini adalah fungsi yang digunakan untuk mendapatkan semua arti berhubungan
-    baik dihalaman ini, atau dihalaman yang lainnya
-    
-    :soup adalah beautifulsoup object
-    :soupping adalah fungsi yang digunakan untuk membuat beautifulsoup object
-    yang merupakan kombinasi membuka url dan beautifulsoup atau 
-    mebuka file dan beautifulsoup
-    """
-    
-    url = get_next_page_url(soup)
-    result = get_arti_berhub(soup)
-    while url:
-        soup = soupping(url)
-        url = get_next_page_url(soup)
-        arti_berhub = get_arti_berhub(soup)
-        result += arti_berhub
-    return result
+    def get_all_arti_berhub(self, soupping):
+        """
+        ini adalah fungsi yang digunakan untuk mendapatkan semua arti berhubungan
+        baik dihalaman ini, atau dihalaman yang lainnya
+        
+        :soup adalah beautifulsoup object
+        :soupping adalah fungsi yang digunakan untuk membuat beautifulsoup object
+        yang merupakan kombinasi membuka url dan beautifulsoup atau 
+        mebuka file dan beautifulsoup
+        """
+        
+        url = self.get_next_page_url()
+        result = self.get_arti_berhub()
+        while url:
+            self.soup = soupping(url)
+            url = self.get_next_page_url()
+            arti_berhub = self.get_arti_berhub()
+            result += arti_berhub
+        return result
     
 
 
@@ -77,44 +77,44 @@ class AraIndParserTest(unittest.TestCase):
 
     def setUp(self):
         self.soup = self.soupping(self.get_abs_path('html/rumah+sakit.html'))
-        indaraparser = IndAraParser(self.soup)
+        self.indaraparser = IndAraParser(self.soup)
     
     def test_get_master_tranlated(self):
-        master = get_ara_master(self.soup)
+        master = self.indaraparser.get_ara_master()
         self.assertEqual(master, "مستشفى")
 
     def test_get_master_ind(self):
-        master = get_ind_master(self.soup)
+        master = self.indaraparser.get_ind_master()
         self.assertEqual(master, "rumah sakit")
 
     def test_get_footer_translation(self):
-        master = get_footer_master(self.soup)
+        master = self.indaraparser.get_footer_master()
         self.assertEqual(master, "*Diterjemahkan dengan Bing Translator ")
 
     def test_get_arti_berhub_jumlah(self):
-        secondary = get_arti_berhub(self.soup)
+        secondary = self.indaraparser.get_arti_berhub()
         self.assertEqual(len(secondary), 10)
 
     def test_get_arti_berhub_first(self):
-        secondary = get_arti_berhub(self.soup)
+        secondary = self.indaraparser.get_arti_berhub()
         self.assertEqual(secondary[0], {"ind":"rumah sakit gila", "ara":"بَيتُ الـمَجَانِبِينِ، مُسْتَشْفَى الـمَجَانِيْنِ"})
     
     def test_get_arti_master(self):
         "memberikan kembalian berupa dict, {'ind' : indonesia, 'ara': arabic, 'footer': footer_text}"
-        master = get_arti_master(self.soup)
+        master = self.indaraparser.get_arti_master()
         self.assertEqual(master, {"ind":"rumah sakit", "ara":"مستشفى", "footer":"*Diterjemahkan dengan Bing Translator "})
     
     def test_get_next_page_url(self):
-        url_to = get_next_page_url(self.soup)
+        url_to = self.indaraparser.get_next_page_url()
         self.assertEqual(url_to, self.get_abs_path("html/rumah+sakit2.html"))
 
     def test_get_next_page_url_with_no_next_in_page(self):
         soup = self.soupping(self.get_abs_path("html/rumah+sakit9.html"))
-        no_next = get_next_page_url(soup)
+        no_next = IndAraParser(soup).get_next_page_url() 
         self.assertFalse(no_next)
 
     def test_get_all_kata_berhub(self):
-        secondary = get_all_arti_berhub(self.soup, self.soupping)
+        secondary = self.indaraparser.get_all_arti_berhub(self.soupping)
         self.assertEqual(len(secondary), 89)
 
 
