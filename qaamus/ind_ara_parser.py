00c2671319e9771e24d890de0d6e2887d73d5b73
ihfazhillah <mihfazhillah@gmail.com>
2016-04-22 23:13:22 +0700

    refactor and commenting

--- qaamus/ind_ara_parser.py
+++ qaamus/ind_ara_parser.py
@@ -1,50 +1,64 @@
 from bs4 import BeautifulSoup
 
 
-class IndAraParser:
-
+class IndAraParser(object):
+    """Class untuk parsing halaman terjemah 
+    indonesia arab."""
 
     def __init__(self, soup):
+        """Inisiasi soup objek"""
         self.soup = soup
 
-    def get_ara_master(self):
-        result = self.soup.select("center > .lateef2")
-        return result[0].text
+    def _get_ara_master(self):
+        """Return arti utama."""
+        return self.soup.select("center > .lateef2")[0].text
 
-    def get_ind_master(self):
-        result = self.soup.select(".panel-heading > h3 > .label")
-        return result[0].text
+    def _get_ind_master(self):
+        """Return kata yang dicari."""
+        return self.soup.select(".panel-heading > h3 > .label")[0].text
 
-    def get_footer_master(self):
-        result = self.soup.select(".panel-footer")
-        return result[0].text
-
-    def get_arti_berhub(self):
-        ind = [x.text for x in self.soup.select("td > a")]
-        ara = [x.text for x in self.soup.select("td.lateef")]
-        result =[{"ind":ind[i], "ara":ara[i]}for i in range(len(ind))] 
-        return result
-
+    def _get_footer_master(self):
+        """Return footer pencarian, bisa jadi pencarian menggunakan
+        Bing translator sehingga ditampilkan di web."""
+        return self.soup.select(".panel-footer")[0].text
+    
     def get_arti_master(self):
-        result = {"ind": self.get_ind_master(),
-                  "ara": self.get_ara_master(),
-                  "footer": self.get_footer_master()
-                 }
-        return result
+        """Return dictionary hasil pencarian dengan kata-kunci 
+        *ind* untuk pencarian,
+        *ara* untuk hasil pencarian,
+        *footer* ditampilkan ketika pencarian menggunakan Bing translator."""
+        return {"ind": self._get_ind_master(),
+                "ara": self._get_ara_master(),
+                "footer": self._get_footer_master()
+               }
+
+    def get_arti_berhub(self, soup=None):
+        """Return list of dictionary berupa arti berhubungan
+        dengan arti utama dengan **kata-kunci**
+        *ind* adalah indonesia
+        *ara* adalah arti arabnya."""
+        if soup is not None:
+            soup = self.soup
+
+        ind = [x.text for x in soup.select("td > a")]
+        ara = [x.text for x in soup.select("td.lateef")]
+        return [{"ind":ind[i], "ara":ara[i]}for i in range(len(ind))] 
 
     def get_next_page_url(self):
+        """Return url next page bila program menemukan *Next »*,
+        else: return *False*."""
         #http://stackoverflow.com/questions/9007653/how-to-find-tag-with-particular-text-with-beautiful-soup
         find_next = self.soup.find("a", text="Next »")
         if find_next:
             return find_next['href']
         return False
 
-    def get_all_arti_berhub(self, soupping):
+    def get_all_arti_berhub(self, make_soup):
         """
         ini adalah fungsi yang digunakan untuk mendapatkan semua arti berhubungan
         baik dihalaman ini, atau dihalaman yang lainnya
         
-        :soupping adalah fungsi yang digunakan untuk membuat beautifulsoup object
+        :make_soup adalah fungsi yang digunakan untuk membuat beautifulsoup object
         yang merupakan kombinasi membuka url dan beautifulsoup atau 
         mebuka file dan beautifulsoup
         """
@@ -52,10 +66,8 @@ class IndAraParser:
         url = self.get_next_page_url()
         result = self.get_arti_berhub()
         while url:
-            self.soup = soupping(url)
+            soup = make_soup(url)
             url = self.get_next_page_url()
-            arti_berhub = self.get_arti_berhub()
-            result += arti_berhub
+            result += self.get_arti_berhub(soup)
         return result
     
-
