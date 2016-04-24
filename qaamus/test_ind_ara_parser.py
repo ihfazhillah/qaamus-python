a885c0ca15e423b8ce4d1625a19fdc5e8cf342f5
ihfazhillah <mihfazhillah@gmail.com>
2016-04-25 00:19:16 +0700

    refactor

--- qaamus/test_ind_ara_parser.py
+++ qaamus/test_ind_ara_parser.py
@@ -5,9 +5,8 @@ from ind_ara_parser import IndAraParser
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 class AraIndParserTest(unittest.TestCase):
-
-
     def get_abs_path(self, path):
         return os.path.join(BASE_DIR, path)
 
@@ -19,7 +18,7 @@ class AraIndParserTest(unittest.TestCase):
     def setUp(self):
         self.soup = self.soupping(self.get_abs_path('html/rumah+sakit.html'))
         self.indaraparser = IndAraParser(self.soup)
-    
+
     def test_get_master_tranlated(self):
         master = self.indaraparser.get_ara_master()
         self.assertEqual(master, "مستشفى")
@@ -38,20 +37,25 @@ class AraIndParserTest(unittest.TestCase):
 
     def test_get_arti_berhub_first(self):
         secondary = self.indaraparser.get_arti_berhub()
-        self.assertEqual(secondary[0], {"ind":"rumah sakit gila", "ara":"بَيتُ الـمَجَانِبِينِ، مُسْتَشْفَى الـمَجَانِيْنِ"})
-    
+        self.assertEqual(secondary[0],
+                         {"ind": "rumah sakit gila",
+                          "ara": "بَيتُ الـمَجَانِبِينِ، مُسْتَشْفَى الـمَجَانِيْنِ"})
+
     def test_get_arti_master(self):
-        "memberikan kembalian berupa dict, {'ind' : indonesia, 'ara': arabic, 'footer': footer_text}"
+        """memberikan kembalian berupa dict, {'ind': indonesia,
+        'ara': arabic, 'footer': footer_text}"""
         master = self.indaraparser.get_arti_master()
-        self.assertEqual(master, {"ind":"rumah sakit", "ara":"مستشفى", "footer":"*Diterjemahkan dengan Bing Translator "})
-    
+        self.assertEqual(master, {"ind": "rumah sakit",
+                                  "ara": "مستشفى",
+                                  "footer": "*Diterjemahkan dengan Bing Translator "})
+
     def test_get_next_page_url(self):
         url_to = self.indaraparser.get_next_page_url()
         self.assertEqual(url_to, self.get_abs_path("html/rumah+sakit2.html"))
 
     def test_get_next_page_url_with_no_next_in_page(self):
         soup = self.soupping(self.get_abs_path("html/rumah+sakit9.html"))
-        no_next = IndAraParser(soup).get_next_page_url() 
+        no_next = IndAraParser(soup).get_next_page_url()
         self.assertFalse(no_next)
 
     def test_get_all_kata_berhub(self):
@@ -61,4 +65,3 @@ class AraIndParserTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
