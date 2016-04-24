bf9ad6c5ddaa284938ee0b89f1330993317ae41a
ihfazhillah <mihfazhillah@gmail.com>
2016-04-24 23:08:40 +0700

    Refactoring...

--- qaamus/qaamus.py
+++ qaamus/qaamus.py
@@ -3,19 +3,23 @@ import requests
 from bs4 import BeautifulSoup
 from ind_ara_parser import IndAraParser
 
+
 class Qaamus:
-
-
     def terjemah(self, layanan, query):
+        """
+        Return terjemahan tergantung dengan **layanan** apa yang dia pilih,
+        dan **query** apa yang dia pakai.
+        Adapun *layanan* di [Qaamus](qaamus.com) saat ini terdapat 3 layanan:
+            * Indonesia Arab
+            * Angka
+            * Terjemah nama
+        Sedangkan *query* adalah query pencarian anda"""
         if layanan == "idar":
             url = self.build_idar_url(query)
             soup = self._make_soup(url)
             parser = IndAraParser(soup)
-            return {"utama" : parser.get_arti_master(),
-                    "berhubungan" : parser.get_all_arti_berhub(self._make_soup)
-                   }
-
-
+            return {"utama": parser.get_arti_master(),
+                    "berhubungan": parser.get_all_arti_berhub(self._make_soup)}
 
     def _make_soup(self, url):
         """Return BeautifulSoup object."""
@@ -23,13 +27,13 @@ class Qaamus:
         return BeautifulSoup(resp.content)
 
     def build_idar_url(self, query):
-        """Return url pencarian sesuai dengan *query* yang dimasukkan.""" 
+        """Return url pencarian sesuai dengan *query* yang dimasukkan."""
         query = "+".join(query.split(" "))
         url = "http://qaamus.com/indonesia-arab/" + query + "/1"
         return url
 
-class QaamusTest(unittest.TestCase):
 
+class QaamusTest(unittest.TestCase):
 
     def test_building_idar_url(self):
         q = Qaamus()
@@ -42,7 +46,6 @@ class QaamusTest(unittest.TestCase):
         expected_url = "http://qaamus.com/indonesia-arab/mobil+ambulan+bagus/1"
         this_url = q.build_idar_url("mobil ambulan bagus")
         self.assertEqual(this_url, expected_url)
-
 
 
 if __name__ == "__main__":
