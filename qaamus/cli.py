53055b32bb78777bd0f4b129a3279ebae643be2f
ihfazhillah <mihfazhillah@gmail.com>
2016-04-29 23:39:18 +0700

    membuat cli mode untuk idar, keluaran masih berupa dict

---
+++ qaamus/cli.py
@@ -0,0 +1,28 @@
+import argparse
+import qaamus
+
+
+def add_parser():
+    parser = argparse.ArgumentParser(description="Terjemah Indonesia Arab")
+    parser.add_argument(
+            "query",
+            metavar="Query",
+            help="Sebuah query yang ingin dicari artinya")
+    parser.add_argument(
+            "-i", "--idar",
+            action="store_true",
+            help="Mengartikan ke Bahasa Arab dari suatu query")
+    return parser
+
+
+def get_parser(parser):
+    return parser.parse_args()
+
+
+def main():
+    namespace = get_parser(add_parser())
+    if namespace.idar:
+        print(qaamus.idar(namespace.query))
+
+if __name__ == "__main__":
+    main()
