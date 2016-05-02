93f0d8ef9f4996adbd4289dd629624cadd01ded4
ihfazhillah <mihfazhillah@gmail.com>
2016-05-03 01:20:12 +0700

    add angka function

--- qaamus/api.py
+++ qaamus/api.py
@@ -3,3 +3,7 @@ from qaamus import Qaamus
 
 def idar(query, pretty=False):
     return Qaamus().terjemah("idar", query, pretty)
+
+
+def angka(query, pretty=False):
+    return Qaamus().terjemah("angka", query, pretty)
