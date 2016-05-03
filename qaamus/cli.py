662df6386e073b93495aa77c15563d12715157ad
ihfazhillah <mihfazhillah@gmail.com>
2016-05-04 01:11:23 +0700

    delet query, add angka-instruction option

--- qaamus/cli.py
+++ qaamus/cli.py
@@ -5,17 +5,19 @@ import api
 def add_parser():
     parser = argparse.ArgumentParser(description="Terjemah Indonesia Arab")
     parser.add_argument(
-            "query",
-            metavar="Query",
-            help="Sebuah query yang ingin dicari artinya")
-    parser.add_argument(
             "-i", "--idar",
-            action="store_true",
+            action="store",
+            metavar="INDONESIA",
             help="Mengartikan ke Bahasa Arab dari suatu query")
     parser.add_argument(
             "-a", "--angka",
-            action="store_true",
+            action="store",
+            metavar="ANGKA",
             help="Mengartikan query berupa angka kedalam bahasa arab")
+    parser.add_argument(
+           "-A", "--angka-instruction",
+           action="store_true",
+           help="Instruksi layanan terjemah angka")
     return parser
 
 
@@ -26,9 +28,13 @@ def get_parser(parser):
 def main():
     namespace = get_parser(add_parser())
     if namespace.idar:
-        print(api.idar(namespace.query, pretty=True))
+        print(api.idar(namespace.idar, pretty=True))
+
     elif namespace.angka:
-        print(api.angka(namespace.query, pretty=True))
+        print(api.angka(namespace.angka, pretty=True))
+
+    elif namespace.angka_instruction:
+        print(api.angka_instruction(pretty=True))
 
 if __name__ == "__main__":
     main()
