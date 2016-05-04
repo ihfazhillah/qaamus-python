f989c6f74391fa6afda6a3e47212c848f34b202a
ihfazhillah <mihfazhillah@gmail.com>
2016-05-05 00:29:51 +0700

    cleaning code and refactor

--- qaamus/qaamus.py
+++ qaamus/qaamus.py
@@ -177,29 +177,37 @@ class Qaamus:
         return BeautifulSoup(resp.content)
 
     def build_url(self, query, layanan=None):
-        """Return url pencarian sesuai dengan *query* yang dimasukkan."""
+        """Return url pencarian sesuai dengan *query* yang dimasukkan.
+        layanan ketika None maka akan terjadi pengecekan query,
+        kalau query adalah integer atau seperti integer maka berikan kembalian
+        ANGKA_URL,
+        kalau string maka secara default akan direfrensikan ke INDO_URL.
+        Bila tidak None, maka tergantung dengan layanan yang diminta."""
+
         # syntactic sugar
         is_angka = isinstance(query, int) or query.isdigit()
 
+        ANGKA_URL = "http://qaamus.com/terjemah-angka/{number}/angka".format(
+            number=query)
+        INDO_URL = "http://qaamus.com/indonesia-arab/{query}/1".format(
+            query="+".join(query.split()))
+        PEGON_URL = 'http://qaamus.com/terjemah-nama/{pegon}'.format(
+            pegon=query)
+
+        layanan_mapping = dict(
+            (('angka', ANGKA_URL),
+             ('idar', INDO_URL),
+             ('pegon', PEGON_URL))
+            )
+
         if not layanan:
             if is_angka:
-                url = "http://qaamus.com/terjemah-angka/{number}/angka".format(
-                    number=query)
+                url = ANGKA_URL
             elif isinstance(query, str):
-                query = "+".join(query.split(" "))
-                url = "http://qaamus.com/indonesia-arab/{query}/1".format(
-                        query=query)
+                url = INDO_URL
         else:
-            if layanan == 'pegon':
-                url = 'http://qaamus.com/terjemah-nama/{pegon}'.format(
-                    pegon=query)
-            elif layanan == 'idar':
-                query = "+".join(query.split(" "))
-                url = "http://qaamus.com/indonesia-arab/{query}/1".format(
-                        query=query)
-            elif layanan == 'angka':
-                url = "http://qaamus.com/terjemah-angka/{number}/angka".format(
-                    number=query)
+            if layanan in layanan_mapping:
+                url = layanan_mapping[layanan]
             else:
                 raise LayananValueError("Layanan tidak ditemukan")
 
