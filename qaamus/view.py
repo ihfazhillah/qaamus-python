513465f4d3a489e27511301fe26b0ba7e1b02d06
ihfazhillah <mihfazhillah@gmail.com>
2016-05-10 23:08:48 +0700

    add instruksi view

--- qaamus/view.py
+++ qaamus/view.py
@@ -1,9 +1,24 @@
 import unittest
 
 
+class SampleObject(object):
+    instruksi = "Ini adalah instruksi"
+
+
 class View(object):
+    def __init__(self, object_=None):
+        self.object_ = object_
+
     def render(self):
-        return "No object found."
+        if self.object_ is None:
+            return "No object found."
+        else:
+            if hasattr(self.object_, "instruksi"):
+                instruksi = getattr(self.object_, "instruksi")
+                header = ("###\n"
+                          "#Instruksi\n"
+                          "###\n")
+                return "\n".join([header, instruksi])
 
 
 class ViewTestCase(unittest.TestCase):
@@ -11,6 +26,11 @@ class ViewTestCase(unittest.TestCase):
         view = View()
         rendered = view.render()
         self.assertEqual(rendered, "No object found.")
+
+    def test_only_instruction_is_not_none(self):
+        view = View(SampleObject)
+        rendered = view.render()
+        self.assertEqual(rendered, "###\n#Instruksi\n###\n\nIni adalah instruksi")
 
 
 if __name__ == "__main__":
