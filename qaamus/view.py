f23a94a192058a55af56ce923554f8fd7f878724
ihfazhillah <mihfazhillah@gmail.com>
2016-05-11 20:41:16 +0700

    if berhubungan not None, and add Instruksi template

--- qaamus/view.py
+++ qaamus/view.py
@@ -6,6 +6,7 @@ class SampleObject(object):
     query = None
     ara = None
     footer = None
+    berhubungan = None
 
 
 class SampleObjectUtama(object):
@@ -13,6 +14,22 @@ class SampleObjectUtama(object):
     query = "coba"
     ara = "coba"
     footer = "Ini footer"
+    berhubungan = None
+
+
+class SampleObjectIdAr(object):
+    instruksi = None
+    query = "coba"
+    ara = "coba"
+    footer = "Ini footer"
+    berhubungan = [("a", "b"), ("c", "d")]
+
+
+INSTRUKSI_TEMPLATE = """###
+#Instruksi
+###
+
+{instruksi}"""
 
 
 class View(object):
@@ -26,12 +43,12 @@ class View(object):
             instruksi = getattr(self.object_, "instruksi")
             utama = [getattr(self.object_, x) for x in
                      ["query", "ara", "footer"]]
+            berhubungan = getattr(self.object_, "berhubungan")
+
             if instruksi:
-                header = ("###\n"
-                          "#Instruksi\n"
-                          "###\n")
-                return "\n".join([header, instruksi])
-            elif all(utama):
+                return INSTRUKSI_TEMPLATE.format(instruksi=instruksi)
+
+            elif all(utama) and not getattr(self.object_, "berhubungan"):
                 template = ("###\n"
                             "#Arti dari {query}\n"
                             "###\n\n"
@@ -43,6 +60,25 @@ class View(object):
                     query=utama[0],
                     ara=utama[1],
                     footer=utama[2])
+
+            elif all(utama) and berhubungan:
+                template = ("###\n"
+                            "#Arti dari {query}\n"
+                            "###\n\n"
+                            "{ara}\n"
+                            "\n{footer}"
+                            "\n\n"
+                            "###\n"
+                            "#Arti berhubungan dari {query}\n"
+                            "###\n"
+                            )
+                berhubungan_ = ["%s : %s" % (x[0], x[1]) for x in berhubungan]
+                berhubungan = "\n".join(berhubungan_)
+
+                return template.format(
+                    query=utama[0],
+                    ara=utama[1],
+                    footer=utama[2]) + berhubungan
 
 
 class ViewTestCase(unittest.TestCase):
@@ -65,6 +101,22 @@ class ViewTestCase(unittest.TestCase):
                     "###\n\n"
                     "coba\n"
                     "\nIni footer")
+        self.assertEqual(rendered, expected)
+
+    def test_query_ara_footer_with_berhubungan_is_not_none(self):
+        view = View(SampleObjectIdAr)
+        rendered = view.render()
+        expected = ("###\n"
+                    "#Arti dari coba\n"
+                    "###\n\n"
+                    "coba\n"
+                    "\nIni footer"
+                    "\n\n"
+                    "###\n"
+                    "#Arti berhubungan dari coba\n"
+                    "###\n"
+                    "a : b\n"
+                    "c : d")
         self.assertEqual(rendered, expected)
 
 
