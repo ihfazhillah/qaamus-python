c6c40dcde604091c3890588192112f3297594222
ihfazhillah <mihfazhillah@gmail.com>
2016-05-11 21:59:15 +0700

    separate between view and test_view

---
+++ qaamus/test_view.py
@@ -0,0 +1,74 @@
+import unittest
+from view import View
+
+
+class SampleObject(object):
+    instruksi = "Ini adalah instruksi"
+    query = None
+    ara = None
+    footer = None
+    berhubungan = None
+
+
+class SampleObjectUtama(object):
+    instruksi = None
+    query = "coba"
+    ara = "coba"
+    footer = "Ini footer"
+    berhubungan = None
+
+
+class SampleObjectIdAr(object):
+    instruksi = None
+    query = "coba"
+    ara = "coba"
+    footer = "Ini footer"
+    berhubungan = [("a", "b"), ("c", "d")]
+
+
+class ViewTestCase(unittest.TestCase):
+    def test_empty_template(self):
+        view = View()
+        rendered = view.render()
+        self.assertEqual(rendered, "No object found.")
+
+    def test_empty_object(self):
+        view = View("default_template")
+        rendered = view.render()
+        self.assertEqual(rendered, "No object found.")
+
+    def test_only_instruction_is_not_none(self):
+        view = View("default_template")
+        rendered = view.render(SampleObject)
+        self.assertEqual(rendered,
+                         "###\n#Instruksi\n###\n\nIni adalah instruksi")
+
+    def test_query_ara_footer_is_not_none(self):
+        view = View("default_template")
+        rendered = view.render(SampleObjectUtama)
+        expected = ("###\n"
+                    "#Arti dari coba\n"
+                    "###\n\n"
+                    "coba\n"
+                    "\nIni footer")
+        self.assertEqual(rendered, expected)
+
+    def test_query_ara_footer_with_berhubungan_is_not_none(self):
+        view = View("default_template")
+        rendered = view.render(SampleObjectIdAr)
+        expected = ("###\n"
+                    "#Arti dari coba\n"
+                    "###\n\n"
+                    "coba\n"
+                    "\nIni footer"
+                    "\n\n"
+                    "###\n"
+                    "#Arti berhubungan dari coba\n"
+                    "###\n"
+                    "a : b\n"
+                    "c : d")
+        self.assertEqual(rendered, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
