7c7d0236d8009b91374cf8a0ead6484817e512e4
ihfazhillah <mihfazhillah@gmail.com>
2016-05-13 23:52:33 +0700

    adding router to controller

--- qaamus/controller.py
+++ qaamus/controller.py
@@ -1,29 +1,41 @@
 import view
 import parsers
+from router import QaamusRouter
+
+
+router = QaamusRouter()
 
 
 def idar_controller(soup_object, make_soup):
     data = parsers.IndAraParser(soup_object).get_idar(make_soup)
     rendered = view.View().render(data)
     return rendered
+router.register("idar", idar_controller)
+
 
 def angka_controller(soup_object):
     data = parsers.AngkaParser(soup_object).get_arti_master()
     rendered = view.View().render(data)
     return rendered
+router.register("angka", angka_controller)
+
 
 def pegon_controller(soup_object):
     data = parsers.PegonParser(soup_object).get_arti_master()
     rendered = view.View().render(data)
     return rendered
+router.register("pegon", pegon_controller)
+
 
 def angka_instruksi_controller(soup_object):
     data = parsers.AngkaParser(soup_object).get_instruction()
     rendered = view.View().render(data, layanan="Angka")
     return rendered
+router.register("angka_instruksi", angka_instruksi_controller)
+
 
 def pegon_instruksi_controller(soup_object):
     data = parsers.PegonParser(soup_object).get_instruction()
     rendered = view.View().render(data, layanan="Pegon")
     return rendered
-
+router.register("pegon_instruksi", pegon_instruksi_controller)
