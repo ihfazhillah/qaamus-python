7939cd561030c86a727d74d98cf70b5254824a73
ihfazhillah <mihfazhillah@gmail.com>
2016-05-15 21:38:13 +0700

    fix import error

--- qaamus/__init__.py
+++ qaamus/__init__.py
@@ -1,6 +1,6 @@
 from qaamus.qaamus import Qaamus
-from qaamus.api import (idar, angka, angka_instruction,
-                 pegon, pegon_instruction)
+from qaamus.api import (idar, angka, angka_instruksi,
+                 pegon, pegon_instruksi)
 
 
 __version__ = "2.0"
