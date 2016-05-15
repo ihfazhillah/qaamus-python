21f8b2d0ed91983ec179db012dd2a2481bc70300
ihfazhillah <mihfazhillah@gmail.com>
2016-05-15 21:40:07 +0700

    idar, angka, pegon soup moved here

---
+++ tests/utils.py
@@ -0,0 +1,6 @@
+from qaamus.utils import soupping, get_abs_path
+
+
+idar_soup = soupping(get_abs_path("html/rumah+sakit"))
+angka_soup = soupping(get_abs_path("html/angka123"))
+pegon_soup = soupping(get_abs_path("html/pegon_suratman"))
